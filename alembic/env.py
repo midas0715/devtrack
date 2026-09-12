69edd8d0b54c2352ea2fc84b50c493cbac2cad39
midas0715 <prathamsarin0@gmail.com>
2026-09-12 11:33:56 +0530

    Add Issue model with foreign key, create issues table migration

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from alembic import context
 from devtrack.database.base import Base
 from devtrack.models.project import Project
 from devtrack.database.session import engine
+from devtrack.models.issue import Issue
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ src/devtrack/models/issue.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy import Integer,String,DateTime,func,ForeignKey
+from datetime import datetime
+from typing import Optional,Literal
+from devtrack.database.base import Base
+
+class Issue(Base):
+    __tablename__="issues"
+    id: Mapped[int]=mapped_column(primary_key=True)
+    project_id: Mapped[int]=mapped_column(ForeignKey("projects.id"))
+    title: Mapped[str]=mapped_column(String)
+    description: Mapped[str]=mapped_column(String)
+    assignee: Mapped[Optional[str]]=mapped_column(String, nullable=True)
+    status: Mapped[str]=mapped_column(String)
+    priority: Mapped[str]=mapped_column(String)
+    creator: Mapped[str]=mapped_column(String)
+    created_at: Mapped[datetime]=mapped_column(DateTime,server_default=func.now())
+    
