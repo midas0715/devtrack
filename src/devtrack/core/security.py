692f0ea642181ecfc586a0dba9537e7d072f0863
midas0715 <prathamsarin0@gmail.com>
2026-09-15 14:58:22 +0530

    Add JWT token creation and verification

--- src/devtrack/core/security.py
+++ src/devtrack/core/security.py
@@ -1,4 +1,12 @@
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta, timezone
+import os
+from dotenv import load_dotenv
+load_dotenv()
+SECRET_KEY=os.getenv("SECRET_KEY")
+ALGORITHM='HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context= CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -6,3 +14,12 @@ def hash_password(password:str)->str:
     return pwd_context.hash(password)
 def verify_password(password:str,hashed_password:str)->bool:
     return pwd_context.verify(password,hashed_password)
+
+def create_access_token(data:dict):
+    to_encode=data.copy()
+    expire= datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token:str):
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
